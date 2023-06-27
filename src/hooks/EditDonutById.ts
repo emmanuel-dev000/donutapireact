7f54cba6467f0643037ca608fa1fd3042554710c
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-27 11:33:40 +0800

    Updated for deployment

--- src/hooks/EditDonutById.ts
+++ src/hooks/EditDonutById.ts
@@ -3,8 +3,8 @@ import { Donut } from "../types/Donut";
 
 export default async function EditDonutById(id: string, updatedDonut: Donut) {
     const { data } = await
-    // axios.patch("https://donutapiv2-production.up.railway.app/api/v2/donuts")
-        axios.patch(`http://localhost:8080/api/v2/donuts/${ id }`, {
+    axios.patch(`https://donutapiv2-production.up.railway.app/api/v2/donuts/${ id }`, {
+        // axios.patch(`http://localhost:8080/api/v2/donuts/${ id }`, {
             name: updatedDonut.name,
             description: updatedDonut.description,
             imageUrl: updatedDonut.imageUrl,
--- src/hooks/GetDonutById.ts
+++ src/hooks/GetDonutById.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { Donut } from "../types/Donut";
 
 export default async function GetDonutById(id: string) : Promise<Donut> {
-    const { data } = 
-        // axios.get(`https://donutapiv2-production.up.railway.app/api/v2/donuts/${ id }`)
-        await axios.get(`http://localhost:8080/api/v2/donuts/${ id }`);
+    const { data } = await
+        axios.get(`https://donutapiv2-production.up.railway.app/api/v2/donuts/${ id }`);
+        // axios.get(`http://localhost:8080/api/v2/donuts/${ id }`);
     return data;
 }
