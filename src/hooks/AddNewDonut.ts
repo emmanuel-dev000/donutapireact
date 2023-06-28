7982caf9e351b8c89ea85bc976ded62a310e4290
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-28 17:54:53 +0800

    Japanese translation deploy

--- src/hooks/AddNewDonut.ts
+++ src/hooks/AddNewDonut.ts
@@ -3,8 +3,8 @@ import { Donut } from "../types/Donut";
 
 export default async function AddNewDonut(donut: Donut) {
     const { data } =
-        // await axios.post("https://donutapiv2-production.up.railway.app/api/v2/donuts", {
-        await axios.post("http://localhost:8080/api/v2/donuts", {
+        await axios.post("https://donutapiv2-production.up.railway.app/api/v2/donuts", {
+        // await axios.post("http://localhost:8080/api/v2/donuts", {
             id: donut.id,
             name: donut.name,
             description: donut.description,
--- src/hooks/GetDonutById.ts
+++ src/hooks/GetDonutById.ts
@@ -3,7 +3,7 @@ import { Donut } from "../types/Donut";
 
 export default async function GetDonutById(id: string) : Promise<Donut> {
     const { data } = await
-        // axios.get(`https://donutapiv2-production.up.railway.app/api/v2/donuts/${ id }`);
-        axios.get(`http://localhost:8080/api/v2/donuts/${ id }`);
+        axios.get(`https://donutapiv2-production.up.railway.app/api/v2/donuts/${ id }`);
+        // axios.get(`http://localhost:8080/api/v2/donuts/${ id }`);
     return data;
 }
