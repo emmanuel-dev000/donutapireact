497b9665be133b79734bb72da2a2c32b826eca54
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-06-28 17:27:30 +0800

    Removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,3 @@ export default function App() {
     <PageRoutes />
   );
 }
-
-/*
-REACT USES SPRINGBOOT FOR TRANSLATING LANGUAGE
-
-    <Stack direction="row" spacing={1} alignItems="center" paddingRight={2}>
-            <Typography>English</Typography>
-                <Switch color="default" onClick={
-                  async () => {
-                    
-                        setSwitchToEnglish(!switchToEnglish);
-                        if (switchToEnglish) {
-                          const { data } = await axios.get("http://localhost:8080/api/v2/languages/en");
-                          console.log(data);
-                        } else {
-                          const { data } = await axios.get("http://localhost:8080/api/v2/languages/jp");
-                          console.log(data);
-                        }
-                        
-                    }
-                }/>
-            <Typography>日本語</Typography>
-        </Stack>
-*/
